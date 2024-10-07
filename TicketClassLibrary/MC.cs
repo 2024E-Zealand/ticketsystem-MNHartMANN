ef4ed09270671c32ad721b0646ddf02864e7413f
Marcus Nørregaard Hartmann <mah016@edu.zealand.dk>
2024-10-07 16:49:40 +0200

    MC klassen er lavet og UnitTestet Task 3

---
+++ TicketClassLibrary/MC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary
+{
+    public class MC
+    {
+        /// <summary>
+        /// Gets or sets the license plate of a MC.
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date associated with a MC.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Returns the price of a MC, which is 125.
+        /// </summary>
+        public double Price()
+        {
+            return 125.0;
+        }
+
+        /// <summary>
+        /// Returns the type of vehicle, which is a MC.
+        /// </summary>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ TicketTestProjekt/UnitTest2.cs
@@ -0,0 +1,41 @@
+﻿using TicketClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketTestProjekt
+{
+    [TestClass()]
+    public class MCTests
+    {
+        /// <summary>
+        /// Tests that the Price method returns the price 125.
+        /// </summary>
+        [TestMethod]
+        public void Price_ShouldReturnCorrectPrice()
+        {
+            // Arrange
+            var mc = new MC();
+
+            // Act
+            double result = mc.Price();
+
+            // Assert
+            Assert.AreEqual(125.0, result);
+        }
+
+        /// <summary>
+        /// Tests that the VehicleType method returns "MC".
+        /// </summary>
+        [TestMethod]
+        public void VehicleType_ShouldReturnMC()
+        {
+            // Arrange
+            var mc = new MC();
+
+            // Act
+            string result = mc.VehicleType();
+
+            // Assert
+            Assert.AreEqual("MC", result);
+        }
+    }
+}
